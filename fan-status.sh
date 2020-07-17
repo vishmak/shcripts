7f6cc6abb225c5649819618dde5c709907cb2c5e
Prateek K <vishvamitra.ak@gmail.com>
2020-07-17 17:58:22 +0530

    Add  fan-status.sh

---
+++ fan-status.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+fstat=$(cat /sys/devices/platform/asus-nb-wmi/fan_boost_mode)
+
+case "$fstat" in
+   "0") echo "Fan Mode: Balenced" 
+   ;;
+   "1") echo "Fan Mode: Overboost" 
+   ;;
+   "2") echo "Fan Mode: Silent" 
+   ;;
+   *) echo "Fan Mode: Unknown" 
+esac
